3528c2a0d013bc9e243d691050ef0e942cb50164
LipeFelps <lipefelps2002@gmail.com>
2021-10-10 19:51:11 -0300

    Consumo de API - Login e Cadastro

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConsoleReporter } from 'jasmine';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
+
+
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    window.scroll(0,0)
+  }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+      this.userLogin = resp
+
+      
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+
+      console.log(environment.token)
+
+      console.log(environment.nome)
+
+      console.log(environment.foto)
+
+      console.log(environment.id);
+      
+      
+
+      this.userLogin.foto
+
+      this.router.navigate(['/inicio'])
+    }, error =>{
+      if(error.status == 5000){
+        alert('Usuário ou senha estão incorretos!')
+
+      }
+
+    })
+
   }
 
 }
