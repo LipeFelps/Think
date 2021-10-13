41032d06e0310dd650b477e72166546a7a16946b
LipeFelps <lipefelps2002@gmail.com>
2021-10-13 02:25:49 -0300

    termineeeeeeei

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AlertasService } from './../service/alertas.service';
+import { TemaService } from './../service/tema.service';
+import { Tema } from './../model/Tema';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
-import { Tema } from '../model/Tema';
-import { TemaService } from '../service/tema.service';
+import { environment } from './../../environments/environment.prod';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tema',
@@ -14,34 +15,38 @@ export class TemaComponent implements OnInit {
   tema: Tema = new Tema()
   listaTemas: Tema[]
 
-
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alertas: AlertasService
   ) { }
 
-  ngOnInit(){
+  ngOnInit() {
     if(environment.token == ''){
-    this.router.navigate(['/entrar'])
-  }
-  this.findAllTema()
-  }
-  findAllTema(){
-    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
-      this.listaTemas = resp
+      this.router.navigate(['/entrar'])
     }
-    )
 
+    if(environment.tipo != 'adm'){
+      this.alertas.showAlertInfo('Você precisa ser adm para acessar essa rota')
+      this.router.navigate(['/inicio'])
+    }
+
+    this.findAllTemas()
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
   }
 
   cadastrar(){
-    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
-      this.tema = resp
-      alert('Tema cadastrado com sucesso!')
-      this.findAllTema
-      this.tema = new Tema()
-    })
-
+     this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+       this.tema = resp
+       this.alertas.showAlertSuccess('Tema cadastrado com sucesso!')
+       this.findAllTemas()
+       this.tema = new Tema()
+     })
   }
 
 }
